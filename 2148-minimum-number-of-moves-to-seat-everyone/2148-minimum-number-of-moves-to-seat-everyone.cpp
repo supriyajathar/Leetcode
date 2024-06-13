63d78606e16a8e05a98097e9cf2d0b3134f156f8
Supriya jathar <116477978+supriyajathar@users.noreply.github.com>
2024-06-13 23:15:26 +0530

    Time: 3 ms (91.43%), Space: 21 MB (52.06%) - LeetHub

--- 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.cpp
+++ 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.cpp
@@ -5,24 +5,29 @@ public:
         sort(seats.begin(),seats.end());
           sort(students.begin(),students.end());
 
-        int i=0;
-        int j=0;
-        while(i<seats.size() && j<students.size())
+        // int i=0;
+        // int j=0;
+        // while(i<seats.size() && j<students.size())
+        // {
+        //     if(seats[i]==students[j])
+        //     {
+        //         i++;
+        //         j++;
+        //     }
+        //     else
+        //     {
+        //         int num=abs(seats[i]-students[j]);
+        //         count=count+num;
+        //         i++;
+        //         j++;
+
+        //     }
+
+        // }
+        // return count;
+        for(int i=0;i<seats.size();i++)
         {
-            if(seats[i]==students[j])
-            {
-                i++;
-                j++;
-            }
-            else
-            {
-                int num=abs(seats[i]-students[j]);
-                count=count+num;
-                i++;
-                j++;
-
-            }
-
+            count=count+abs(seats[i]-students[i]);
         }
         return count;
         
