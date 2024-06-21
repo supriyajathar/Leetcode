b88ab0cc87c3c26c3426e7cf93efaa3f6544532a
Supriya jathar <116477978+supriyajathar@users.noreply.github.com>
2024-06-21 16:50:12 +0530

    Time: 7 ms (84.86%), Space: 15.7 MB (7.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,19 +2,37 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         //first solution 0(n^2)
+        // vector<int>ans;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     for(int j=i+1;j<nums.size();j++)
+        //     {
+        //         if(nums[i]+nums[j]==target)
+        //         {
+        //                ans.push_back(i);
+        //                ans.push_back(j);
+        //         }
+        //     }
+        // }
+        // return ans;
+        //second approach o(n)
+        unordered_map<int,int>mp;
         vector<int>ans;
         for(int i=0;i<nums.size();i++)
         {
-            for(int j=i+1;j<nums.size();j++)
+            int isans=target-nums[i];
+            if(mp.find(isans)!=mp.end())
             {
-                if(nums[i]+nums[j]==target)
-                {
-                       ans.push_back(i);
-                       ans.push_back(j);
-                }
+                ans.push_back(mp[isans]);
+                  ans.push_back(i);
+            }
+            else
+            {
+                mp[nums[i]]=i;
             }
         }
         return ans;
+
 
         
     }
