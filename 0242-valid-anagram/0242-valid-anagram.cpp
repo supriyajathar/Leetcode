172ceb12a56674ea6fbba35c3fa105e9b7891dc4
Supriya jathar <116477978+supriyajathar@users.noreply.github.com>
2024-07-03 23:50:00 +0530

    Time: 6 ms (79.75%), Space: 9.5 MB (39.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -7,16 +7,39 @@ public:
         {
             return false;
         }
-        sort(s.begin(),s.end());
-           sort(t.begin(),t.end());
-           if(s.compare(t)==0)
-           {
-            return true;
-           }
-           else
-           {
-            return false;
-           }
+        // sort(s.begin(),s.end());
+        //    sort(t.begin(),t.end());
+        //    if(s.compare(t)==0)
+        //    {
+        //     return true;
+        //    }
+        //    else
+        //    {
+        //     return false;
+        //    }
+        unordered_map<char,int>mp;
+        for(int i=0;i<s.size();i++)
+        {
+            char ch =s[i];
+            mp[ch]++;
+        }
+        for(int j=0;j<t.size();j++)
+        {
+            char ch =t[j];
+            if(mp.find(ch)!=mp.end() && mp[ch]!=0)
+            {
+                mp[ch]--;
+                continue;
+            }
+
+            else
+            {
+                return false;
+            }
+         
+        }
+        return true;
+
 
         
     }
