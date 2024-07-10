b5dc538a57e2621bfcb3903fa90f041583a79d7d
Supriya jathar <116477978+supriyajathar@users.noreply.github.com>
2024-07-10 23:32:18 +0530

    Time: 4 ms (67.13%), Space: 14.4 MB (17.13%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.cpp
+++ 1720-crawler-log-folder/1720-crawler-log-folder.cpp
@@ -1,24 +1,55 @@
 class Solution {
 public:
     int minOperations(vector<string>& logs) {
-        int depth=0;
+        // int depth=0;
+        // for(int i=0;i<logs.size();i++)
+        // {
+        //     string str=logs[i];
+        //     if(str!="../" && str!="./")
+        //     {
+        //         depth++;
+        //     }
+        //    else if(str=="../" && depth!=0)
+        //    {
+        //     depth--;
+        //    }
+        //    else
+        //    {
+        //     continue;
+        //    }
+        // }
+        // return depth;
+        int count=0;
+        stack<string>st;
         for(int i=0;i<logs.size();i++)
         {
             string str=logs[i];
-            if(str!="../" && str!="./")
+            if(str=="../" )
             {
-                depth++;
+                if(!st.empty())
+                {
+ st.pop();
+                }
+               
             }
-           else if(str=="../" && depth!=0)
-           {
-            depth--;
-           }
-           else
-           {
-            continue;
-           }
+            else if(str=="./")
+            {
+                continue;
+            }
+            else
+            {
+                st.push(str);
+            }
         }
-        return depth;
+      while(!st.empty())
+      {
+        string s=st.top();
+        count++;
+        st.pop();
+      }
+
+
+return count;
         
     }
 };
