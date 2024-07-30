bc7e67a60d45b9cdceb8592f3471c998aa633cd7
Supriya jathar <116477978+supriyajathar@users.noreply.github.com>
2024-07-31 00:28:32 +0530

    Time: 96 ms (51.67%), Space: 24.6 MB (54.55%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
-    int minimumDeletions(string s) {
-        int n = s.length();
-        stack<char>st ; 
-        int count = 0 ;
-        st.push(s[0]);
-        for(int i=1;i<n;i++){
-            if(!st.empty()  && (st.top() == 'b' && s[i] == 'a')){
-                st.pop();
-                count+=1;
-            }
-            else{
-                st.push(s[i]);
-            }
+     int minimumDeletions(string s) {
+        // int n = s.length();
+        // stack<char>st ; 
+        // int count = 0 ;
+        // st.push(s[0]);
+        // for(int i=1;i<n;i++){
+        //     if(!st.empty()  && (st.top() == 'b' && s[i] == 'a')){
+        //         st.pop();
+        //         count+=1;
+        //     }
+        //     else{
+        //         st.push(s[i]);
+        //     }
+        // }
+        // return count ;
+    int n=s.size();
+    stack<char>st;
+    st.push(s[0]);
+    int count=0;
+
+    for(int i=1;i<n;i++)
+    {
+        if(!st.empty() && (st.top()=='b' &&s[i]=='a'))
+        {
+            st.pop();
+            count++;
         }
-        return count ;
+        else
+        {
+            st.push(s[i]);
+        }
+    }
+   return count;
     }
 };
